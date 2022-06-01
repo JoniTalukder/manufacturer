8c38a7cd8e434021c7cd11a535255465ace26526
joniTalukder <jonictg7@gmail.com>
2022-06-01 14:42:12 +0600

    review shows 3 item only

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -9,7 +9,7 @@ const Reviews = () => {
             <h2 className='text-primary text-4xl font-bold uppercase'>Our Review</h2>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
-                    review?.slice(0, 6).map(review => <Review
+                    review?.slice(0, 3).map(review => <Review
                         key={review._id}
                         review={review}
                     ></Review>)
